10b7ba9a03113fd5d37d8a78c4e6e196a2e9509d
Ndumiso Blose <blosendumo@gmail.com>
2026-06-02 16:58:02 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-console.log("Supabase loading...")
+
 
 const supabaseUrl = "https://veslqfjrjfzridzwavzu.supabase.co"
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlc2xxZmpyamZ6cmlkendhdnp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzMzU1MDUsImV4cCI6MjA5NTkxMTUwNX0.Zp9y_cLr0ipTW6NRKVgl74YgTC6XszdrrkHXPJPDoWw"
