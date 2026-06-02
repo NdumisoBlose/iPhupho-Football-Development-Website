dbeb1655c6d5169dd843174f45a61c4152ee10c9
Ndumiso Blose <blosendumo@gmail.com>
2026-06-02 16:20:33 +0200

    Update logout.js

--- js/logout.js
+++ js/logout.js
@@ -1,15 +1,1 @@
-window.addEventListener("DOMContentLoaded", () => {
 
-  const btn = document.getElementById("logoutBtn")
-  if (!btn) return
-
-  btn.addEventListener("click", async () => {
-
-    await window.supabaseClient.auth.signOut()
-
-    localStorage.clear()
-
-    window.location.replace("admin-login.html")
-  })
-
-})
