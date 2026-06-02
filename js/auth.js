ea049e044d703415104dd6672523d611227c592d
Ndumiso Blose <blosendumo@gmail.com>
2026-06-02 16:47:16 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,13 +1,1 @@
-window.addEventListener("load", async () => {
 
-  const session = JSON.parse(
-    localStorage.getItem("session")
-  )
-
-  if (!session) {
-    window.location.href = "admin-login.html"
-    
-  }
-
-  await window.supabaseClient.auth.setSession(session)
-})
